ec7afd980b2e88aa8186eebeb153ddb42dc3bdde
hei14569823 <37259518+hei14569823@users.noreply.github.com>
2018-10-19 17:31:58 +0800

    替换文件
    今天天气好好，于是就替换了文件

--- Boom.java
+++ Boom.java
@@ -1,6 +1,7 @@
 import java.util.Random;
+import java.util.Arrays;
 public class Boom{
-	public void view(int n,int m){
+	public static void view(int n,int m){
 		for(int i=0;i<n;i++){
 			for(int j=0;j<m;j++){
 				System.out.print("*  ");
@@ -8,17 +9,43 @@ public class Boom{
 			System.out.println("");
 		}
 	}
-	public void num(){
-		int[] intArray=new int[2];
+	public static int[][] num(int nums){
+		int[][] intArray=new int[nums][2];
 		Random random=new Random();
-		for(int i=0;i<intArray.length;i++){
-			intArray[i]=random.nextInt(10);
+		for(int i=0;i<nums;i++){
+			for(int j=0;j<2;j++){
+				intArray[i][j]=random.nextInt(10);
+			}
 		}
 		return intArray;
 	}
+	public static int[][] compute(int nums,int k,int z){
+		int[][] intA=num(nums);
+		int[][] comArr=new int[k][z];
+		for(int i=0;i<nums;i++){
+			if(intA[i][0]!=0 && intA[i][1]!=0){
+				int a=intA[i][0],b=intA[i][1];
+				comArr[a][b]++;
+				System.out.println(comArr);
+			}
+		}
+		return null;
+	}
 	public static void main(String[] args) {
-		Boom boom = new Boom();
-		//boom.view(10,9);
-		System.out.println(boom.num());
+		/*Boom boom = new Boom();
+		boom.view(10,9);*/
+		/*boom.num(7);*/
+		
+		// view(10,9);
+		
+		/*int[][] intA=num(7);
+		for(int i=0;i<intA.length;i++){
+			for(int j=0;j<intA[i].length;j++){
+				System.out.print(intA[i][j]+" ");
+			}
+			System.out.println();
+		}*/
+
+		compute(7,10,9);
 	}
 }
